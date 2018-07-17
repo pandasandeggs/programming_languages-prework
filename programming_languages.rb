b99bc737421f2e40064bff20de252510b5528b12
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-17 03:27:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
+languages = {
+  :oo => {
+    :ruby => {:type => "interpreted"},
+    :javascript => {:type => "interpreted"},
+    :python => {:type => "interpreted"},
+    :java => {:type => "compiled"}
+  },
+  
+  :functional => {
+    :clojure => {:type => "compiled"},
+    :erlang => {:type => "compiled"},
+    :scala => {:type => "compiled"},
+    :javascript => {:type => "interpreted"}
+ 
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
-end
+  new_hash ={}
+
+  languages.each do |style,language|
+    if style == [:oo]
+      language[:style] = "oo"
+    elsif style == [:functional]
+      language[:style] = "functional"
+    end
+  end 
+end 
